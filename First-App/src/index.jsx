5393d384096635285c2ce21a9abed1b160908241
andrwcao <80874259+andrwcao@users.noreply.github.com>
2022-07-31 03:12:03 +1000

    Implemented GET jira functionality

--- First-App/src/index.jsx
+++ First-App/src/index.jsx
@@ -1,9 +1,17 @@
-import ForgeUI, { render, ProjectPage, Fragment, Text } from '@forge/ui';
+import ForgeUI, { render, ProjectPage, Fragment, Text, useState } from '@forge/ui';
+import api, { route } from '@forge/api';
+
+const fetchNumberOfIssues = async () => {
+    const response = await api.asApp().requestJira(route`/rest/api/3/search`);
+    const data = await response.json();
+    return data.total;
+}
 
 const App = () => {
+    const [numOfIssues] = useState(async () => await fetchNumberOfIssues());
     return (
         <Fragment>
-            <Text>Hello world! abc</Text>
+            <Text>Number of issues: {numOfIssues}</Text>
         </Fragment>
     );
 };
