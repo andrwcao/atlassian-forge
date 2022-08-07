bf9a1023230190cb2551f2978e34301b65645f73
andrwcao <80874259+andrwcao@users.noreply.github.com>
2022-08-07 17:19:53 +1000

    Compliance Project page implemented

--- JiraComplianceChecker/src/index.js
+++ JiraComplianceChecker/src/index.js
@@ -1,11 +1,43 @@
 import Resolver from '@forge/resolver';
+import api, { route } from '@forge/api';
 
 const resolver = new Resolver();
+const requiredSummary = 50;
+const requiredDescription = 4;
 
-resolver.define('getText', (req) => {
-  console.log(req);
+function getScore(data) {
+  let summaryScore = data.fields.summary ? data.fields.summary.length/requiredSummary : 0;
+  if (summaryScore > 1) {
+    summaryScore = 1;
+  }
 
-  return 'Hello, world!';
+  let descriptionScore = data.fields.description.content ? data.fields.description.content.length/requiredDescription : 0;
+  if (descriptionScore > 1) {
+    descriptionScore = 1;
+  }
+
+  return (summaryScore + descriptionScore) / 2 * 100;
+}
+
+resolver.define('getText', async (req) => {
+  const response = await api.asApp().requestJira(route`/rest/api/3/issue/${req.context.extension.issue.key}`);
+  const data = await response.json();
+  return getScore(data);
+});
+
+resolver.define('getProjectOverview', async (req) => {
+  let jql = `project in (${req.context.extension.project.key})`;
+  const response = await api.asApp().requestJira(route`/rest/api/3/search?jql=${jql}&expand=names&fields=summary,description`);
+  const data = await response.json();
+  let issueScores = [];
+  for (let issue of data.issues) {
+    issueScores.push({
+      'key': issue.key,
+      'scoreText': getScore(issue) + "%"
+    });
+  }
+
+  return issueScores;
 });
 
 export const handler = resolver.getDefinitions();
---
+++ JiraComplianceChecker/static/project-page/src/App.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import { invoke } from '@forge/bridge';
+
+function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    invoke('getProjectOverview', { example: 'my-invoke-variable' }).then(setData);
+  }, []);
+
+  return (
+    <div>
+      <table>
+        <tr>
+          <th>Key</th>
+          <th>Score</th>
+        </tr>
+        {data ? 
+          data.map(issue => (
+            <tr>
+              <td>{issue.key}</td>
+              <td>
+                <svg width="100px" height="10px">
+                  <g class="bars">
+                    <rect fill="#ff0000" width="100%" height="10"></rect>
+                    <rect fill="#00ff00" width={issue.scoreText} height="10"></rect>
+                  </g>
+                </svg>
+              </td>
+            </tr>
+          )) 
+          : 'Loading...'
+        }
+      </table>
+    </div>
+  );
+}
+
+export default App;
